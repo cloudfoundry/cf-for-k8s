37fa9e05cbd03810ef19bd903b865d9499d20854
Andrew Costa <acosta@pivotal.io>
2020-01-28 15:01:06 -0800

    Use new build dir for istio and eirini
    Separate the scripts and templates concerned with building the resources we
    keep in the config directory into the build directory.
    
    [#170321972](https://www.pivotaltracker.com/story/show/170321972)
    
    Co-authored-by: Dmitriy Kalinin <dkalinin@pivotal.io>

--- build/eirini/build.sh
+++ build/eirini/build.sh
@@ -6,5 +6,5 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 EIRINI_RELEASE_PATH="${1:-eirini/eirini}"
 
 echo "generating Eirini resource definitions..."
-helm template --namespace=cf-system "${EIRINI_RELEASE_PATH}" --values="${SCRIPT_DIR}/eirini-values.yml" | ytt --ignore-unknown-comments -f - -f "${SCRIPT_DIR}/add-namespaces-overlay.yml" > "${SCRIPT_DIR}/../config/_ytt_lib/eirini/rendered.yml"
+helm template --namespace=cf-system "${EIRINI_RELEASE_PATH}" --values="${SCRIPT_DIR}/eirini-values.yml" | ytt --ignore-unknown-comments -f - -f "${SCRIPT_DIR}/add-namespaces-overlay.yml" > "${SCRIPT_DIR}/../../config/_ytt_lib/eirini/rendered.yml"
 

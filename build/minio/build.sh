d0ba5848b0c3b033c4433b6dbc7a4810f7027b5f
Jen Spinney <jspinney@pivotal.io>
2020-02-06 16:05:43 -0800

    Pull in minio chart via vendir
    Co-authored-by: John Ryan <jryan@pivotal.io>

--- build/minio/build.sh
+++ build/minio/build.sh
@@ -4,4 +4,4 @@ set -eu
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "Generating Minio resource definitions..."
-helm template cf-blobstore --namespace=cf-blobstore "${SCRIPT_DIR}/minio/" | ytt --ignore-unknown-comments -f - > "${SCRIPT_DIR}/../../config/_ytt_lib/minio/rendered.yml"
+helm template cf-blobstore --namespace=cf-blobstore "${SCRIPT_DIR}/_vendir/stable/minio/" | ytt --ignore-unknown-comments -f - > "${SCRIPT_DIR}/../../config/_ytt_lib/minio/rendered.yml"

22fd25aee32d13437e5be421ebb05b33ae0a049e
Jen Spinney <jspinney@pivotal.io>
2020-02-10 16:36:58 -0800

    Scrubbed default blobstore creds from rendered helm chart.
    Also added a comment explaining how to properly vendir include helm
    chart dependencies.
    
    [#171042234]
    
    Signed-off-by: Jen Spinney <jspinney@pivotal.io>

--- build/minio/build.sh
+++ build/minio/build.sh
@@ -4,4 +4,9 @@ set -eu
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "Generating Minio resource definitions..."
-helm template cf-blobstore --namespace=cf-blobstore "${SCRIPT_DIR}/_vendir/stable/minio/" | ytt --ignore-unknown-comments -f - > "${SCRIPT_DIR}/../../config/_ytt_lib/minio/rendered.yml"
+
+helm template cf-blobstore --namespace=cf-blobstore "${SCRIPT_DIR}/_vendir/stable/minio/" | \
+  ytt --ignore-unknown-comments \
+      -f - \
+      -f "${SCRIPT_DIR}/scrub_default_creds.yml" \
+      > "${SCRIPT_DIR}/../../config/_ytt_lib/minio/rendered.yml"

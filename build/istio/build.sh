08b4cd8a1c3057d8e123f311d4a5d827bd50a536
Pivotal <pivotal-relint@pivotal.io>
2020-02-12 11:51:15 -0800

    Bump istio to 1.4.3

--- build/istio/build.sh
+++ build/istio/build.sh
@@ -2,7 +2,7 @@
 set -eu
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-DESIRED_ISTIO_VERSION="1.4.2"
+DESIRED_ISTIO_VERSION="1.4.3"
 
 istioctl_version="$(istioctl version --remote=false)"
 if [ ${istioctl_version} != "${DESIRED_ISTIO_VERSION}" ]; then

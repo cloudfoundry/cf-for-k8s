f8295830d0798fcd8b9f476107a315bcb16f0561
Dmitriy Kalinin <dkalinin@pivotal.io>
2020-02-18 10:53:35 -0800

    Have vendir pull in Eirini's helm chart
    Instead of manually extracting and copying in Eirini's helm chart from their GitHub release,
    we can now let vendir do that for us. This also makes it more obvious where the Eirini bits
    are coming from.
    
    Requires vendir 0.6.0 and above.
    
    [finishes #171337251]
    
    Signed-off-by: Jen Spinney <jspinney@pivotal.io>

--- build/eirini/build.sh
+++ build/eirini/build.sh
@@ -4,4 +4,4 @@ set -eu
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "generating Eirini resource definitions..."
-helm template --namespace=cf-system "${SCRIPT_DIR}/eirini/" --values="${SCRIPT_DIR}/eirini-values.yml" | ytt --ignore-unknown-comments -f - -f "${SCRIPT_DIR}/add-namespaces-overlay.yml" > "${SCRIPT_DIR}/../../config/_ytt_lib/eirini/rendered.yml"
+helm template --namespace=cf-system "${SCRIPT_DIR}/_vendir/eirini" --values="${SCRIPT_DIR}/eirini-values.yml" | ytt --ignore-unknown-comments -f - -f "${SCRIPT_DIR}/add-namespaces-overlay.yml" > "${SCRIPT_DIR}/../../config/_ytt_lib/eirini/rendered.yml"

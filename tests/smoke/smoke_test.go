8255fd2d40007079c5a25bb44471bbef1ea2e73b
Jen Spinney <jspinney@pivotal.io>
2020-03-04 17:17:03 -0800

    Revert "Revert "Bump capi-k8s-release""
    Accidentally reverted the wrong commits previously.
    
    Please ignore and pretend you haven't seen this...
    
    This reverts commit a2370ba2b91c8c5cda1b95bfa61847271365a2c1.

--- tests/smoke/smoke_test.go
+++ tests/smoke/smoke_test.go
@@ -74,8 +74,7 @@ var _ = Describe("Smoke Tests", func() {
 
 			By("Pushing an app and checking that the CF CLI command succeeds")
 			cfPush := cf.Cf("push", appName, "-o", "cloudfoundry/diego-docker-app")
-			Eventually(cfPush).Should(Exit(1))
-			// HACK: until we have the metrics server deployed, we expect "Stats server will be unavailable."
+			Eventually(cfPush).Should(Exit(0))
 
 			By("Querying the app")
 			var resp *http.Response

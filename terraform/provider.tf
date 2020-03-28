adacb4db747bd4a375f503268ff3ecc1f2244c58
Dave Walter <dwalter@pivotal.io>
2020-03-27 18:39:55 -0700

    Fix issues with the google-beta terraform provider
    - reference vars by name, instead of interpolating them
    
    [#171420608](https://www.pivotaltracker.com/story/show/171420608)

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "google-beta" {
-  project     = "${var.project}"
-  region      = "${var.region}"
-  credentials = "${var.service_account_key}"
+  project     = var.project
+  region      = var.region
+  credentials = var.service_account_key
 }

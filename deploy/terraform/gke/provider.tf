75f06c8103ce2b6e418d4a2f0c79527281f36e91
Dave Walter <dwalter@pivotal.io>
2020-04-10 15:03:26 -0700

    Add a google provider to the terraform templates
    [#172213534](https://www.pivotaltracker.com/story/show/172213534)
    
    Co-authored-by: Andrew Costa <acosta@pivotal.io>
    Co-authored-by: Andrew Wittrock <awittrock@pivotal.io>

--- deploy/terraform/gke/provider.tf
+++ deploy/terraform/gke/provider.tf
@@ -1,3 +1,9 @@
+provider "google" {
+  project     = var.project
+  region      = var.region
+  credentials = var.service_account_key
+}
+
 provider "google-beta" {
   project     = var.project
   region      = var.region

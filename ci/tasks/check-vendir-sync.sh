49767dc300236fd9b90aaf822d341b394f9eb3bd
Eric Promislow <epromislow@suse.com>
2020-04-21 14:59:36 -0700

    Verify `vendor sync` and build images are in sync
    * Verify all necessary COMPONENT/build.sh scripts have been run
    
    * Do this in separate concourse task files rather than inline
    
    [#172444368](https://www.pivotaltracker.com/story/show/172444368)
    
    Signed-off-by: James Pollard <jpollard@pivotal.io>

---
+++ ci/tasks/check-vendir-sync.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -exc
+
+cd cf-for-k8s-repo
+echo "Verifying vendir sync doesn't create new changes..."
+vendir sync > /dev/null
+
+set +e
+git status
+git add .
+git --no-pager diff HEAD
+set -e
+
+git diff-index --quiet HEAD
+echo "Successfully verified running `vendir sync` did not create new changes"
+
+DIR=$PWD
+cd build
+for dir in * ; do
+  if [[ -d "$dir" ]] ; then
+    cd $dir
+    ./build.sh
+    if [[ $dir = "minio" ]] ; then
+      # Ignore the change to the `rollme` random value
+      minio_rendered_file="$DIR/config/_ytt_lib/minio/rendered.yml"
+      if [[ $(git diff --unified=0 "$minio_rendered_file" | egrep "^(\+|-) " | grep -v rollme | wc -l) == 0 ]] ; then
+        git co "$minio_rendered_file"
+      fi
+    fi
+    cd ..
+  fi
+done
+
+set +e
+git status
+git add .
+git --no-pager diff HEAD
+set -e
+
+git diff-index --quiet HEAD
+echo "Successfully verified the build scripts did not create new changes"
+

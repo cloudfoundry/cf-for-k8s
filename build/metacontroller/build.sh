9db790a6c56683a00455c81ed5115911dc2c526f
Paul Warren <pwarren@pivotal.io>
2020-05-21 14:01:21 -0700

    Have kbld override the image not ytt
    [#172898062](https://www.pivotaltracker.com/story/show/172898062)
    
    Signed-off-by: Andrew Wittrock <awittrock@pivotal.io>

--- build/metacontroller/build.sh
+++ build/metacontroller/build.sh
@@ -4,4 +4,4 @@ set -eu
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "generating Metacontroller resource definitions..."
-ytt -f "${SCRIPT_DIR}/osl-compliant-image-override.yml" -f "${SCRIPT_DIR}/_vendir/manifests" | kbld -f - > "${SCRIPT_DIR}/../../config/_ytt_lib/metacontroller/rendered.yml"
+ytt -f "${SCRIPT_DIR}/_vendir/manifests" | kbld -f "${SCRIPT_DIR}/osl-compliant-image-override.yml" -f - > "${SCRIPT_DIR}/../../config/_ytt_lib/metacontroller/rendered.yml"

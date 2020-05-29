a6deae808497af7ec316167ba3f27d30f1b01779
Dave Walter <dwalter@pivotal.io>
2020-05-28 17:13:53 -0700

    Add a wildcard dns record for the apps domain
    - we have seen failures to resolve app urls to IPs likely due to the system domain dns record not catching queries to nested subdomains
    [#173071717](https://www.pivotaltracker.com/story/show/173071717)
    
    Signed-off-by: Paul Warren <pwarren@pivotal.io>

--- deploy/aks/terraform/dns.tf
+++ deploy/aks/terraform/dns.tf
@@ -9,3 +9,15 @@ resource "google_dns_record_set" "wildcard" {
 
   rrdatas = [azurerm_public_ip.lb_static_ip.ip_address]
 }
+
+resource "google_dns_record_set" "apps_wildcard" {
+  name = "*.apps.${var.env_dns_domain}."
+  project = var.google_project
+
+  type = "A"
+  ttl  = 300
+
+  managed_zone = var.dns_zone_name
+
+  rrdatas = [azurerm_public_ip.lb_static_ip.ip_address]
+}
--- deploy/gke/terraform/dns.tf
+++ deploy/gke/terraform/dns.tf
@@ -9,3 +9,15 @@ resource "google_dns_record_set" "wildcard" {
 
   rrdatas = [google_compute_address.lb_static_ip.address]
 }
+
+resource "google_dns_record_set" "apps_wildcard" {
+  name = "*.apps.${var.env_dns_domain}."
+  project = var.project
+
+  type = "A"
+  ttl  = 300
+
+  managed_zone = var.dns_zone_name
+
+  rrdatas = [google_compute_address.lb_static_ip.address]
+}

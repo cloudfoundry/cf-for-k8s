c6224c999d1fb7d1cf59f9a5324daa49e87d65c7
Joseph Palermo <jpalermo@pivotal.io>
2020-06-10 15:42:50 -0700

    Add ability to run uptimer during installation of cf
    [#173164064](https://www.pivotaltracker.com/story/show/173164064)
    
    Signed-off-by: Paul Warren <pwarren@pivotal.io>

--- ci/tasks/install-cf-on-gke/task.sh
+++ ci/tasks/install-cf-on-gke/task.sh
@@ -2,6 +2,38 @@
 
 source cf-for-k8s-ci/ci/helpers/gke.sh
 
+function write_uptimer_deploy_config {
+cat <<EOF > /tmp/uptimer-config.json
+ {
+    "while": [
+        {
+            "command": "kapp",
+            "command_args": ["deploy", "-a", "cf", "-f", "/tmp/manifest.yml", "-y"]
+        }
+    ],
+    "cf": {
+        "api": "api.${DNS_DOMAIN}",
+        "app_domain": "apps.${DNS_DOMAIN}",
+        "admin_user": "admin",
+        "admin_password": "${password}",
+        "tcp_domain": "tcp.${DNS_DOMAIN}",
+        "use_single_app_instance": false,
+        "available_port": 1025
+    },
+    "optional_tests": {
+      "run_app_syslog_availability": false
+    },
+    "allowed_failures": {
+        "app_pushability": 100,
+        "http_availability": 100,
+        "recent_logs": 100,
+        "streaming_logs": 100,
+        "app_syslog_availability": 100
+    }
+}
+EOF
+}
+
 cluster_name="$(cat pool-lock/name)"
 gcloud_auth "${cluster_name}"
 
@@ -19,8 +51,16 @@ cf-for-k8s-repo/hack/generate-values.sh --cf-domain "${DNS_DOMAIN}" --gcr-servic
 echo "istio_static_ip: $(jq -r '.lb_static_ip' pool-lock/metadata)" >> cf-values.yml
 
 echo "Installing CF..."
-kapp deploy -a cf -f <(ytt -f cf-for-k8s-repo/config -f cf-values.yml) -y
+ytt -f cf-for-k8s-repo/config -f cf-values.yml > /tmp/manifest.yml
+password=$(bosh interpolate --path /cf_admin_password cf-values.yml)
+if [[ "${UPTIMER}" == "true" ]]; then
+  echo "Running with uptimer"
+  write_uptimer_deploy_config
+  uptimer -configFile /tmp/uptimer-config.json
+else
+  kapp deploy -a cf -f /tmp/manifest.yml -y
+fi
 
-bosh interpolate --path /cf_admin_password cf-values.yml > env-metadata/cf-admin-password.txt
+echo ${password} > env-metadata/cf-admin-password.txt
 echo "${DNS_DOMAIN}" > env-metadata/dns-domain.txt
 cp "/tmp/${cluster_name}.k8s-dev.relint.rocks/cf-vars.yaml" env-metadata

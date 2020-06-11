c303c66d3d9411b381e6752417c49f0041c84587
Lisa Burns <lcho@pivotal.io>
2020-06-11 11:02:27 -0700

    Make bump-kpack task idempotent
    - if tag does not change, git commit exits 1
    [#173006821](https://www.pivotaltracker.com/story/show/173006821)
    
    Co-authored-by: Lisa Burns <lcho@pivotal.io>
    Co-authored-by: Andrew Costa <acosta@pivotal.io>

--- ci/tasks/bump-core-component/task.sh
+++ ci/tasks/bump-core-component/task.sh
@@ -4,6 +4,13 @@ TAG=$(cat release/tag)
 pushd cf-for-k8s-develop > /dev/null
   CURR_TAG_LINE=$(grep -A 20 "path: .*${REPO_NAME}" vendir.yml | grep tag | head -n1 | awk '{$1=$1;print}')
   sed "s/${CURR_TAG_LINE}/tag: ${TAG}/g" vendir.yml > /tmp/vendir.yml && mv /tmp/vendir.yml vendir.yml
+  
+  CURR_TAG=$(echo $CURR_TAG_LINE | awk '{print $2}')
+
+  if [[ "${CURR_TAG}" == "${TAG}" ]]; then
+    echo "Tag ${CURR_TAG} has not changed. No update needed."
+    exit 0
+  fi
 
   vendir sync
 

b82d26e1e75cabb03e340b6053eca6dadad99710
Alex Standke <astandke@vmware.com>
2020-06-15 19:01:03 +0000

    Delete metacontroller
    - this was only used by the cfroutesync controller which has been
    removed in favor of the new route controller [cf-k8s-networking
    ADR#10](https://github.com/cloudfoundry/cf-k8s-networking/blob/master/doc/architecture-decisions/0010-route-crd-and-kubebuilder-instead-of-metacontroller.md).
    
    [#173165427](https://www.pivotaltracker.com/story/show/173165427)
    
    Co-authored-by: Christian Ang <angc@vmware.com>

--- build/metacontroller/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-echo "generating Metacontroller resource definitions..."
-ytt -f "${SCRIPT_DIR}/_vendir/manifests" | kbld -f "${SCRIPT_DIR}/osl-compliant-image-override.yml" -f - > "${SCRIPT_DIR}/../../config/_ytt_lib/metacontroller/rendered.yml"

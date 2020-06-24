30e02b6d86e7d4bf1809ca437596c7c83b78c197
James Pollard <jpollard@pivotal.io>
2020-06-24 15:50:19 -0700

    Bump capi-k8s-release nightly
    [#173499282](https://www.pivotaltracker.com/story/show/173499282)
    
    Signed-off-by: Eric Promislow <epromislow@suse.com>

--- ci/tasks/bump-core-component/task.sh
+++ ci/tasks/bump-core-component/task.sh
@@ -1,20 +1,28 @@
 #!/bin/bash -eux
 
-TAG=$(cat release/tag)
+if [[ ${REPO_NAME} == "capi-k8s-release" ]]; then
+  pushd release > /dev/null
+    TAG=$(git rev-parse HEAD)
+  popd > /dev/null
+else
+  TAG=$(cat release/tag)
+fi
+
 pushd cf-for-k8s-develop > /dev/null
-  if [[ ${REPO_NAME} == "uaa" ]] || [[ ${REPO_NAME} == "cf-k8s-networking" ]]; then
+
+  if [[ ${REPO_NAME} =~ ^(capi-k8s-release|uaa|cf-k8s-networking)$ ]]; then
     # we believe we need the component teams to include the necessary files in their release assets
     # before we can switch to using a githubRelease in vendir.yml
     vendir_key="ref"
   else
     vendir_key="tag"
   fi
-  CURR_TAG_LINE=$(grep -A 20 "path: .*${REPO_NAME}" vendir.yml | grep ${vendir_key} | head -n1 | awk '{$1=$1;print}')
-  sed "s/${CURR_TAG_LINE}/${vendir_key}: ${TAG}/g" vendir.yml > /tmp/vendir.yml && mv /tmp/vendir.yml vendir.yml
-  
-  CURR_TAG=$(echo $CURR_TAG_LINE | awk '{print $2}')
 
-  if [[ "${CURR_TAG}" != "${TAG}" ]]; then
+  CURR_TAG_LINE=$(grep -A 20 "path: .*${REPO_NAME}" vendir.yml | grep ${vendir_key} | head -n1 | awk '{$1=$1;print}')
+  OLD_TAG=$(echo $CURR_TAG_LINE | awk '{print $2}')
+  sed "s/${CURR_TAG_LINE}/${vendir_key}: ${TAG}/g" vendir.yml > /tmp/vendir.yml && mv /tmp/vendir.yml vendir.yml
+
+  if [[ "${OLD_TAG}" != "${TAG}" ]]; then
     vendir sync
 
     git config user.email "cf-release-integration@pivotal.io"
@@ -22,7 +30,7 @@ pushd cf-for-k8s-develop > /dev/null
     git add .
     git commit -m "Bump ${REPO_NAME} to ${TAG}"
   else
-    echo "Tag ${CURR_TAG} has not changed. No update needed."
+    echo "Tag ${OLD_TAG} has not changed. No update needed."
   fi
 
 popd > /dev/null

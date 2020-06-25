640bc53bf74f90ee578175f714a5cb18798451b8
Andrew Costa <acosta@pivotal.io>
2020-06-25 15:31:56 -0700

    Move uptimer-config function to helper
    Co-authored-by: Lisa Burns <lcho@pivotal.io>

---
+++ ci/helpers/uptimer-config.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function write_uptimer_deploy_config {
+cat <<EOF > /tmp/uptimer-config.json
+ {
+    "while": [
+        {
+            "command": "kapp",
+            "command_args": ["deploy", "-a", "cf", "-f", "/tmp/manifest.yml", "-y"]
+        }
+    ],
+    "cf": {
+        "api": "api.${DNS_DOMAIN}",
+        "app_domain": "apps.${DNS_DOMAIN}",
+        "admin_user": "admin",
+        "admin_password": "${password}",
+        "tcp_domain": "tcp.${DNS_DOMAIN}",
+        "use_single_app_instance": false,
+        "available_port": 1025
+    },
+    "optional_tests": {
+      "run_app_syslog_availability": false
+    },
+    "allowed_failures": {
+        "app_pushability": 100,
+        "http_availability": 100,
+        "recent_logs": 100,
+        "streaming_logs": 100,
+        "app_syslog_availability": 100
+    }
+}
+EOF
+}
--- ci/tasks/install-cf-on-gke/task.sh
+++ ci/tasks/install-cf-on-gke/task.sh
@@ -1,38 +1,7 @@
 #!/bin/bash -eu
 
 source cf-for-k8s-ci/ci/helpers/gke.sh
-
-function write_uptimer_deploy_config {
-cat <<EOF > /tmp/uptimer-config.json
- {
-    "while": [
-        {
-            "command": "kapp",
-            "command_args": ["deploy", "-a", "cf", "-f", "/tmp/manifest.yml", "-y"]
-        }
-    ],
-    "cf": {
-        "api": "api.${DNS_DOMAIN}",
-        "app_domain": "apps.${DNS_DOMAIN}",
-        "admin_user": "admin",
-        "admin_password": "${password}",
-        "tcp_domain": "tcp.${DNS_DOMAIN}",
-        "use_single_app_instance": false,
-        "available_port": 1025
-    },
-    "optional_tests": {
-      "run_app_syslog_availability": false
-    },
-    "allowed_failures": {
-        "app_pushability": 100,
-        "http_availability": 100,
-        "recent_logs": 100,
-        "streaming_logs": 100,
-        "app_syslog_availability": 100
-    }
-}
-EOF
-}
+source cf-for-k8s-ci/ci/helpers/uptimer-config.sh
 
 cluster_name="$(cat pool-lock/name)"
 gcloud_auth "${cluster_name}"

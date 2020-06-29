0c5479f2c1088c4024ff6c09b8834db3e9b18aa0
Andrew Costa <acosta@pivotal.io>
2020-06-29 14:34:35 -0700

    Update bump-core-component CI task
    - use params to determine whether it is operating with github releases
    or not and whether to run a build script
    
    [#173499904](https://www.pivotaltracker.com/story/show/173499904)
    
    Co-authored-by: Dave Walter <dwalter@pivotal.io>

--- ci/tasks/bump-core-component/task.sh
+++ ci/tasks/bump-core-component/task.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
-if [[ ${REPO_NAME} == "capi-k8s-release" ]]; then
+if [[ "${GITHUB_RELEASE}" == "false" ]]; then
   pushd release > /dev/null
     TAG=$(git rev-parse HEAD)
   popd > /dev/null
@@ -9,8 +9,7 @@ else
 fi
 
 pushd cf-for-k8s-develop > /dev/null
-
-  if [[ ${REPO_NAME} =~ ^(capi-k8s-release|uaa|cf-k8s-networking)$ ]]; then
+  if [[ "${VENDIR_GITHUB_RELEASE}" == "false" ]]; then
     # we believe we need the component teams to include the necessary files in their release assets
     # before we can switch to using a githubRelease in vendir.yml
     vendir_key="ref"
@@ -19,20 +18,26 @@ pushd cf-for-k8s-develop > /dev/null
   fi
 
   CURR_TAG_LINE=$(grep -A 20 "path: .*${REPO_NAME}" vendir.yml | grep ${vendir_key} | head -n1 | awk '{$1=$1;print}')
-  OLD_TAG=$(echo $CURR_TAG_LINE | awk '{print $2}')
   sed "s/${CURR_TAG_LINE}/${vendir_key}: ${TAG}/g" vendir.yml > /tmp/vendir.yml && mv /tmp/vendir.yml vendir.yml
 
-  if [[ "${OLD_TAG}" != "${TAG}" ]]; then
+  CURR_TAG=$(echo $CURR_TAG_LINE | awk '{print $2}')
+
+  if [[ "${CURR_TAG}" != "${TAG}" ]]; then
     vendir sync
+
+    if [[ -n "${BUILD_DIR}" ]]; then
+      pushd "${BUILD_DIR}" > /dev/null
+        ./build.sh
+      popd > /dev/null
+    fi
 
     git config user.email "cf-release-integration@pivotal.io"
     git config user.name "relint-ci"
     git add .
     git commit -m "Bump ${REPO_NAME} to ${TAG}"
   else
-    echo "Tag ${OLD_TAG} has not changed. No update needed."
+    echo "Tag ${CURR_TAG} has not changed. No update needed."
   fi
-
 popd > /dev/null
 
 cp -r cf-for-k8s-develop/. cf-for-k8s-bump

d168a2e84b5a9228bd1410157d3d9d0c15789329
Andrew Costa <acosta@pivotal.io>
2020-06-30 15:34:00 -0700

    Add task file for inlined create-tf-vars task
    [#172156787](https://www.pivotaltracker.com/story/show/172156787)
    
    Co-authored-by: James Pollard <jpollard@pivotal.io>

---
+++ ci/tasks/create-tf-vars-file/task.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eou pipefail
+
+env_suffix=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+
+echo "ci-test-cluster-${env_suffix}" > tf-vars/env-name.txt
+cat <<EOT > tf-vars/input.tfvars
+project = "${GCP_PROJECT_NAME}"
+region = "${GCP_PROJECT_REGION}"
+zone = "${GCP_PROJECT_ZONE}"
+service_account_key = "$(echo ${SERVICE_ACCOUNT_JSON} | jq -c '.' | sed -e 's#"#\\"#g' -e 's#\\n#\\\\n#g')"
+machine_type = "n1-standard-8"
+EOT

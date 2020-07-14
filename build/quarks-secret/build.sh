7b43637ba3666ba680b2da3b6298780d5e61932a
Tom Kennedy <ktom@vmware.com>
2020-07-14 10:33:04 -0700

    Add quarks secret
    Co-authored-by: Peter Chen <pchen@pivotal.io>
    Co-authored-by: Aidan Obley <aobley@vmware.com>

---
+++ build/quarks-secret/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -eu
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+echo "generating QuarksSecret resource definitions..."
+
+helm template cf-quarks-secret --namespace=cf-system "${SCRIPT_DIR}/_vendir/deploy/helm/quarks-secret" \
+  --values="${SCRIPT_DIR}/quarks-values.yaml" |
+  ytt --ignore-unknown-comments -f - |
+  kbld -f - > "${SCRIPT_DIR}/../../config/_ytt_lib/quarks-secret/rendered.yml"

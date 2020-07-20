d9848fa5f8e44f20ef42ac58a7d61ebd9c2a10f5
James Pollard <jpollard@pivotal.io>
2020-07-20 16:04:32 -0700

    CI-FIX: only push changes if cf-vars file has changed
    [#173779110](https://www.pivotaltracker.com/story/show/173779110)

--- ci/helpers/env-git.sh
+++ ci/helpers/env-git.sh
@@ -4,12 +4,22 @@ commit-long-lived-sli-cf-vars() {
 
     cp -r relint-envs/. relint-envs-updated
     mkdir -p relint-envs-updated/k8s-environments/long-lived-sli
-    cp "/tmp/${DNS_DOMAIN}/cf-vars.yaml" relint-envs-updated/k8s-environments/long-lived-sli/cf-vars.yaml
+    cf_vars_rel_path="k8s-environments/long-lived-sli/cf-vars.yaml"
+    cp "/tmp/${DNS_DOMAIN}/cf-vars.yaml" relint-envs-updated/${cf_vars_rel_path}
 
     pushd relint-envs-updated > /dev/null
-    git config user.email "cf-release-integration@pivotal.io"
-    git config user.name "relint-ci"
-    git add .
-    git commit -m "Update long-lived environment"
+        set +e
+        git diff --exit-code ${cf_vars_rel_path} > /dev/null
+        error_code=$?
+        set -e
+
+        if [[ ${error_code} != 0 ]]; then
+            echo "Committing changes to relint-envs/${cf_vars_rel_path}"
+            git config user.email "cf-release-integration@pivotal.io"
+            git config user.name "relint-ci"
+            git add .
+            git commit -m "Update long-lived environment"
+        fi
+
     popd > /dev/null
 }

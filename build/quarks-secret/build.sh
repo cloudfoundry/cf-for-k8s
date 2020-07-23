378cf454e29fbc14e0fca08d6bfe6e3c17b842ad
Dave Walter <dwalter@pivotal.io>
2020-07-23 16:51:33 -0700

    Refactor quarks-secret into its own component directory
    [#173754832](https://www.pivotaltracker.com/story/show/173754832)
    
    Signed-off-by: Paul Warren <pwarren@pivotal.io>

--- build/quarks-secret/build.sh
+++ build/quarks-secret/build.sh
@@ -8,4 +8,8 @@ echo "generating QuarksSecret resource definitions..."
 helm template cf-quarks-secret --namespace=cf-system "${SCRIPT_DIR}/_vendir/deploy/helm/quarks-secret" \
   --values="${SCRIPT_DIR}/quarks-values.yaml" |
   ytt --ignore-unknown-comments -f - |
+<<<<<<< HEAD
   kbld -f - > "${SCRIPT_DIR}/../../config/quarks-secret/_ytt_lib/quarks-secret/rendered.yml"
+=======
+  kbld -f - > "${SCRIPT_DIR}/../../config/quarks-secret/_ytt_lib/quarks-secret/rendered.yml"
+>>>>>>> 9b53ca7... Refactor quarks-secret into its own component directory

3cda364171f1c31f5cf450f57f6474ef6da793e8
Connor Braa <cbraa@pivotal.io>
2020-07-24 14:10:58 -0700

    rename kpack-watcher-client-secret to cf-api-controllers-client-secret
    - in accordance with https://github.com/cloudfoundry/capi-k8s-release/blob/rename-kpack-watcher-to-controllers/decisions/0003-cf-api-controller-repo.md
    
    Authored-by: Connor Braa <cbraa@pivotal.io>

--- hack/generate-values.sh
+++ hack/generate-values.sh
@@ -105,7 +105,7 @@ variables:
   type: password
 - name: cc_username_lookup_client_secret
   type: password
-- name: kpack_watcher_client_secret
+- name: cf_api_controllers_client_secret
   type: password
 - name: default_ca
   type: certificate
@@ -223,7 +223,7 @@ cf_db:
 
 capi:
   cc_username_lookup_client_secret: $(bosh interpolate ${VARS_FILE} --path=/cc_username_lookup_client_secret)
-  kpack_watcher_client_secret: $(bosh interpolate ${VARS_FILE} --path=/kpack_watcher_client_secret)
+  cf_api_controllers_client_secret: $(bosh interpolate ${VARS_FILE} --path=/cf_api_controllers_client_secret)
   database:
     password: $(bosh interpolate ${VARS_FILE} --path=/capi_db_password)
     encryption_key: $(bosh interpolate ${VARS_FILE} --path=/capi_db_encryption_key)

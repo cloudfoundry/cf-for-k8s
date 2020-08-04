51863329278ab32b4c500766eb628e8123eb29d0
Lisa Burns <lcho@pivotal.io>
2020-08-04 16:31:57 -0700

    Commit both cf-values and cf-vars for long lived environment
    [#174071422](https://www.pivotaltracker.com/story/show/174071422)
    
    Co-authored-by: Lisa Burns <lcho@pivotal.io>
    Co-authored-by: Nancy Hsieh <nhsieh@vmware.com>

--- ci/helpers/env-git.sh
+++ ci/helpers/env-git.sh
@@ -5,16 +5,18 @@ commit-long-lived-sli-cf-vars() {
     cp -r relint-envs/. relint-envs-updated
     mkdir -p relint-envs-updated/k8s-environments/long-lived-sli
     cf_vars_rel_path="k8s-environments/long-lived-sli/cf-vars.yaml"
+    cf_values_rel_path="k8s-environments/long-lived-sli/cf-values.yaml"
     cp "/tmp/${DNS_DOMAIN}/cf-vars.yaml" relint-envs-updated/${cf_vars_rel_path}
+    cp "cf-install-values.yml" relint-envs-updated/${cf_values_rel_path}
 
     pushd relint-envs-updated > /dev/null
         set +e
-        git diff --exit-code ${cf_vars_rel_path} > /dev/null
+        git diff --exit-code ${cf_values_rel_path} ${cf_vars_rel_path} > /dev/null
         error_code=$?
         set -e
 
         if [[ ${error_code} != 0 ]]; then
-            echo "Committing changes to relint-envs/${cf_vars_rel_path}"
+            echo "Committing changes to relint-envs long-lived sli values & vars"
             git config user.email "cf-release-integration@pivotal.io"
             git config user.name "relint-ci"
             git add .

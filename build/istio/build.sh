a77998094396a78c43e9dcb2c2a31276b867108b
Mikael Manukyan <mmanukyan@vmware.com>
2020-09-10 18:56:27 +0000

    Move istio stuff out of the library folder
    * Create new values file for Istio related values
    * Move all istio related templates to config/networking/istio
    * Refactor some networking templates into smaller ones
    
    Co-authored-by: Nitya Dhanushkodi <ndhanushkodi@pivotal.io>

--- build/istio/build.sh
+++ build/istio/build.sh
@@ -3,8 +3,8 @@ set -eu
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-${SCRIPT_DIR}/generate.sh "$@" | kbld -f - > "${SCRIPT_DIR}/../../config/networking/_ytt_lib/istio/istio-generated/xxx-generated-istio.yaml"
+${SCRIPT_DIR}/generate.sh "$@" | kbld -f - > "${SCRIPT_DIR}/../../config/networking/istio/istio-generated/xxx-generated-istio.yaml"
 
 # save the current Istio version in the networking configs
-ISTIO_VERSION="$(< "${SCRIPT_DIR}/values.yml" yq -r .istio_version)"
-sed -r -i'' 's/(istio_version:)(.*)$/\1 "'${ISTIO_VERSION}'"/' "${SCRIPT_DIR}/../../config/networking/_ytt_lib/istio/values.yml"
+ISTIO_VERSION="$(< "${SCRIPT_DIR}/values.yaml" yq -r .istio_version)"
+sed -r -i'' 's/(build_version:)(.*)$/\1 "'${ISTIO_VERSION}'"/' "${SCRIPT_DIR}/../../config/values/30-istio-values.yml"

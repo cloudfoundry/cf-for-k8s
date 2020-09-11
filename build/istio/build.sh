f7b5fe679a3bb94614273614fdb0d1f977770ea8
Nitya Dhanushkodi <ndhanushkodi@pivotal.io>
2020-09-11 18:00:03 +0000

    download istioctl when required in generate
    Co-authored-by: Alex Standke <astandke@vmware.com>
    Co-authored-by: Nitya Dhanushkodi <ndhanushkodi@pivotal.io>

--- build/istio/build.sh
+++ build/istio/build.sh
@@ -3,7 +3,7 @@ set -eu
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-${SCRIPT_DIR}/generate.sh "$@" | kbld -f - > "${SCRIPT_DIR}/../../config/networking/istio/istio-generated/xxx-generated-istio.yaml"
+${SCRIPT_DIR}/generate.sh "$@" | kbld -f - > "${SCRIPT_DIR}/../../config/istio/istio-generated/xxx-generated-istio.yaml"
 
 # save the current Istio version in the networking configs
 ISTIO_VERSION="$(< "${SCRIPT_DIR}/values.yaml" yq -r .istio_version)"

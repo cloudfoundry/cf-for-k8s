ec60854ccdc2114272d7fdd330ef43b7e8a18267
Johannes Dillmann <j.dillmann@sap.com>
2020-09-11 13:34:09 +0200

    Add s3 terraform setup
    Co-authored-by: Johannes Dillmann <j.dillmann@sap.com>

---
+++ deploy/s3/terraform/blobstore.tf
@@ -0,0 +1,19 @@
+resource "aws_s3_bucket" "packages" {
+  bucket =  "cc-packages-${var.bucket_suffix}"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket" "droplets" {
+  bucket =  "cc-droplets-${var.bucket_suffix}"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket" "resources" {
+  bucket =  "cc-resources-${var.bucket_suffix}"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket" "buildpacks" {
+  bucket =  "cc-buildpacks-${var.bucket_suffix}"
+  acl    = "private"
+}
---
+++ deploy/s3/terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "bucket_suffix" {
+  type = string
+}
+
+variable "aws_access_key_id" {
+  type = string
+}
+
+variable "aws_secret_access_key" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}

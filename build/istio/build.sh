4e5d7d74af3de98867fb315ed04a2d6dc78f937d
Nitya Dhanushkodi <ndhanushkodi@pivotal.io>
2020-09-11 19:14:20 +0000

    eliminate need for 30-istio-values.yml
    Co-authored-by: Alex Standke <astandke@vmware.com>

--- build/istio/build.sh
+++ build/istio/build.sh
@@ -7,4 +7,4 @@ ${SCRIPT_DIR}/generate.sh "$@" | kbld -f - > "${SCRIPT_DIR}/../../config/istio/i
 
 # save the current Istio version in the networking configs
 ISTIO_VERSION="$(< "${SCRIPT_DIR}/values.yaml" yq -r .istio_version)"
-sed -r -i'' 's/(build_version:)(.*)$/\1 "'${ISTIO_VERSION}'"/' "${SCRIPT_DIR}/../../config/values/30-istio-values.yml"
+sed -r -i'' 's/(return)(.*)$/\1 "'${ISTIO_VERSION}'"/' "${SCRIPT_DIR}/../../config/istio/upgrade-istio-sidecars-job.yml"

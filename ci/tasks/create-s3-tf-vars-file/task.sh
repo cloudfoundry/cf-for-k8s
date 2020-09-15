35ded67883f070078fcd804e4d33c685f4d3e1d0
Luca Schimweg <luca.schimweg@sap.com>
2020-09-15 11:32:14 +0200

    Adapt according to feedback
    Co-authored-by: Johannes Dillmann <j.dillmann@sap.com>

---
+++ ci/tasks/create-s3-tf-vars-file/task.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -eu
+
+SUFFIX=$(openssl rand -hex 6)
+echo "Using suffix $SUFFIX"
+
+echo "ci-test-cluster-${SUFFIX}" > tf-vars-s3/env-name.txt
+
+cat <<EOT > tf-vars-s3/input.tfvars
+region = "$AWS_REGION"
+aws_access_key_id = "$AWS_ACCESS_KEY_ID"
+aws_secret_access_key = "$AWS_SECRET_ACCESS_KEY"
+bucket_suffix = "$SUFFIX"
+EOT

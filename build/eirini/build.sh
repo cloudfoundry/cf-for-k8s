0442d6130aa43938a7139138deb2324f93aee96d
James Pollard <pollardja@vmware.com>
2020-10-15 14:05:49 -0600

    REF: rename eirini build overlays and place them in a folder
    Co-authored-by: Travis Patterson <travisp@vmware.com>

--- build/eirini/build.sh
+++ build/eirini/build.sh
@@ -7,6 +7,6 @@ echo "generating Eirini resource definitions..."
 helm template --namespace=cf-system "${SCRIPT_DIR}/_vendir/eirini" \
     --values="${SCRIPT_DIR}/eirini-values.yml" |
     ytt --ignore-unknown-comments -f - \
-        -f "${SCRIPT_DIR}/add-namespaces-overlay.yml" |
+        -f "${SCRIPT_DIR}/overlays" |
     kbld -f "${SCRIPT_DIR}/osl-compliant-image-override.yml" -f - \
         >"${SCRIPT_DIR}/../../config/eirini/_ytt_lib/eirini/rendered.yml"

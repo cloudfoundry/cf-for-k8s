ac85b42aa7e755563ecd4fdf6298f31eeda06e77
Dave Walter <walterda@vmware.com>
2020-10-16 13:43:55 -0700

    CI-ENH: Update delete-cf task
    ... to work with terraformed clusters as well as pool clusters

--- ci/tasks/delete-cf/task.sh
+++ ci/tasks/delete-cf/task.sh
@@ -2,7 +2,26 @@
 
 source cf-for-k8s-ci/ci/helpers/gke.sh
 
-cluster_name="$(cat pool-lock/name)"
+if [[ -d pool-lock ]]; then
+  if [[ -d tf-vars ]]; then
+    echo "You may not specify both pool-lock and tf-vars"
+    exit 1
+  fi
+  cluster_name="$(cat pool-lock/name)"
+  load_balancer_static_ip="$(jq -r '.lb_static_ip' pool-lock/metadata)"
+elif [[ -d tf-vars ]]; then
+  if [[ -d terraform ]]; then
+    cluster_name="$(cat tf-vars/env-name.txt)"
+    load_balancer_static_ip="$(jq -r '.lb_static_ip' terraform/metadata)"
+  else
+    echo "You must provide both tf-vars and terraform inputs together"
+    exit 1
+  fi
+else
+  echo "You must provide either pool-lock or tf-vars"
+  exit 1
+fi
+
 gcloud_auth "${cluster_name}"
 
 kapp delete -a cf --yes

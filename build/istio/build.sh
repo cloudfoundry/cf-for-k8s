3f14432a917ab0672bde7dd4a633a2a4a7e84c69
Mikael Manukyan <mmanukyan@vmware.com>
2020-10-20 21:17:18 +0000

    Deploy Contour when ingress_solution_provider is "contour"
    TODO: ccdb pod will fail to finish because of hard dependancy on Istio.
    See https://github.com/cloudfoundry/capi-k8s-release/issues/87
    
    * add Contour deployment templates
    * fix Istio build scripts (fix generated YAML output path after
    refactoring)
    * remove CAPI tcp readiness probes until they are fixed. See https://github.com/cloudfoundry/capi-k8s-release/issues/91
    
    [#175344671](https://www.pivotaltracker.com/story/show/175344671)
    
    Co-authored-by: Kauana dos Santos <kdossantos@vmware.com>

--- build/istio/build.sh
+++ build/istio/build.sh
@@ -3,8 +3,10 @@ set -eu
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-${SCRIPT_DIR}/generate.sh "$@" | kbld -f - > "${SCRIPT_DIR}/../../config/istio/istio-generated/xxx-generated-istio.yaml"
+${SCRIPT_DIR}/generate.sh "$@" | kbld -f - > \
+  "${SCRIPT_DIR}/../../config/ingress/_ytt_lib/istio/istio-generated/xxx-generated-istio.yaml"
 
 # save the current Istio version in the networking configs
 ISTIO_VERSION="$(< "${SCRIPT_DIR}/values.yaml" yq -r .istio_version)"
-sed -r -i'' 's/(return)(.*\..*\..*)$/\1 "'${ISTIO_VERSION}'"/' "${SCRIPT_DIR}/../../config/istio/upgrade-istio-sidecars-job.yml"
+sed -r -i'' 's/(return)(.*\..*\..*)$/\1 "'${ISTIO_VERSION}'"/' \
+  "${SCRIPT_DIR}/../../config/ingress/_ytt_lib/istio/templates/upgrade-istio-sidecars-job.yaml"

2dd0b7beffe3c66914390498fc0c6ead77e55da4
Tim Downey <tdowney@vmware.com>
2020-10-28 10:44:54 -0700

    add cf_api_backup_metadata_generator UAA client
    - this client will be used by the cf-api-backup-metadata-container
    to scrape some metadata about the CF installation to include as
    part of a Velero backup
    
    [#175469923](https://www.pivotaltracker.com/story/show/175469923)
    
    Authored-by: Tim Downey <tdowney@vmware.com>

--- hack/generate-values.sh
+++ hack/generate-values.sh
@@ -109,6 +109,8 @@ variables:
   type: password
 - name: cf_api_controllers_client_secret
   type: password
+- name: cf_api_backup_metadata_generator_client_secret
+  type: password
 - name: default_ca
   type: certificate
   options:
@@ -177,6 +179,7 @@ cf_db:
 capi:
   cc_username_lookup_client_secret: $(bosh interpolate ${VARS_FILE} --path=/cc_username_lookup_client_secret)
   cf_api_controllers_client_secret: $(bosh interpolate ${VARS_FILE} --path=/cf_api_controllers_client_secret)
+  cf_api_backup_metadata_generator_client_secret: $(bosh interpolate ${VARS_FILE} --path=/cf_api_backup_metadata_generator_client_secret)
   database:
     password: $(bosh interpolate ${VARS_FILE} --path=/capi_db_password)
     encryption_key: $(bosh interpolate ${VARS_FILE} --path=/capi_db_encryption_key)

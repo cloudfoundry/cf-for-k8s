d924f4d7218f03b7ddd2278073dd20a090a3bfbe
Mikael Manukyan <mmanukyan@vmware.com>
2020-11-03 21:57:33 +0000

    Move build/istio and build/contour to a build/ingress
    * By having Istio and Contour in one folder will allow us to have shared
    configuration between them
    
    Co-authored-by: Clay Kauzlaric <ckauzlaric@vmware.com>
    Co-authored-by: Mikael Manukyan <mmanukyan@vmware.com>

---
+++ build/ingress/istio/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -eu
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+${SCRIPT_DIR}/generate.sh "$@" | kbld -f - > \
+  "${SCRIPT_DIR}/../../../config/ingress/_ytt_lib/istio/istio-generated/xxx-generated-istio.yaml"
+
+# save the current Istio version in the networking configs
+ISTIO_VERSION="$(< "${SCRIPT_DIR}/values.yaml" yq -r .istio_version)"
+sed -r -i'' 's/(return)(.*\..*\..*)$/\1 "'${ISTIO_VERSION}'"/' \
+  "${SCRIPT_DIR}/../../../config/ingress/_ytt_lib/istio/templates/upgrade-istio-sidecars-job.yaml"

20797e35c2ddd1a1bc3d60a81890e2efb81574f3
Dave Walter <walterda@vmware.com>
2020-11-12 22:28:40 -0800

    CI-ENH: Use a unique name for terraformed S3 buckets
    [#175653017](https://www.pivotaltracker.com/story/show/175653017)

--- ci/tasks/create-s3-tf-vars-file/task.sh
+++ ci/tasks/create-s3-tf-vars-file/task.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 set -eu
 
 SUFFIX=$(openssl rand -hex 6)
-echo "Using suffix $SUFFIX"
+echo "Using suffix ${SUFFIX}"
 
-echo "ci-test-cluster-${SUFFIX}" > tf-vars-s3/env-name.txt
+echo "ci-s3-buckets-${SUFFIX}" > tf-vars-s3/env-name.txt
 
 cat <<EOT > tf-vars-s3/input.tfvars
-region = "$AWS_REGION"
-aws_access_key_id = "$AWS_ACCESS_KEY_ID"
-aws_secret_access_key = "$AWS_SECRET_ACCESS_KEY"
-bucket_suffix = "$SUFFIX"
+region = "${AWS_REGION}"
+aws_access_key_id = "${AWS_ACCESS_KEY_ID}"
+aws_secret_access_key = "${AWS_SECRET_ACCESS_KEY}"
+bucket_suffix = "${SUFFIX}"
 EOT

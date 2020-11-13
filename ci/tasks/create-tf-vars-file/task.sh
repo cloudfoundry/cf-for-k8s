8e8c0204f9134a4f6dfd8d93eef11e90ebe75c6f
Dave Walter <walterda@vmware.com>
2020-11-12 22:31:09 -0800

    CI-ENH: Use a unique name for terraformed "kind on GCP" VMs
    [#175653017](https://www.pivotaltracker.com/story/show/175653017)

--- ci/tasks/create-tf-vars-file/task.sh
+++ ci/tasks/create-tf-vars-file/task.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 set -eu
 
 # Don't -o pipefail for this part
 env_suffix=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 
-echo "ci-test-cluster-${env_suffix}" > tf-vars/env-name.txt
+echo "${CLUSTER_NAME}-${env_suffix}" > tf-vars/env-name.txt
 cat <<EOT > tf-vars/input.tfvars
 project = "${GCP_PROJECT_NAME}"
 region = "${GCP_PROJECT_REGION}"

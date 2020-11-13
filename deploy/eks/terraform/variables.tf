117f9c5c3b05e20fb7a6f0dba5cb9df43eebcf69
Dave Walter <walterda@vmware.com>
2020-11-12 18:13:37 -0800

    Add kubectl_config output to EKS templates
    - add descriptions to all variables
    
    [#175653017](https://www.pivotaltracker.com/story/show/175653017)

--- deploy/eks/terraform/variables.tf
+++ deploy/eks/terraform/variables.tf
@@ -1,27 +1,31 @@
 variable "access_key_id" {
-  description = "Access Key ID"
+  description = "AWS access key ID"
   type = string
 }
 
 variable "secret_access_key" {
+  description = "AWS secret access key"
   type = string
 }
 
 variable "region" {
-  default     = "us-west-2"
   description = "AWS region"
   type = string
+  default     = "us-west-2"
 }
 
 variable "azs" {
+  description = "Availability zones for the configured region"
   default = ["us-west-2a","us-west-2b","us-west-2c","us-west-2d"]
 }
 
 variable "private_subnets" {
+  description = "A list of private subnets, one per configured availability zone"
   default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "public_subnets" {
+  description = "A list of public subnets, one per configured availability zone"
   default = ["10.0.5.0/24", "10.0.6.0/24", "10.0.7.0/24", "10.0.8.0/24"]
 }
 
@@ -31,17 +35,19 @@ variable "env_name" {
 }
 
 variable "node_count" {
+  description = "Number of desired Kubernetes nodes"
   type = number
   default = 5
 }
 
 variable "node_machine_type" {
+  description = "Kubernetes node machine type"
   type = string
   default = "t3.xlarge"
 }
 
 variable "eks_version" {
+  description = "Kubernetes cluster version"
   type = string
   default = "1.18"
-  description = "K8s cluster version"
 }

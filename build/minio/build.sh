7abaafa5d2fe760959635c76c680554f9dfddbe6
Dave Walter <walterda@vmware.com>
2020-11-30 12:39:16 -0800

    Bump minio to 8.0.5
    * Source is now mino repo, not helm repo, because helm github
    repo is deprecated
    
    [#175806310](https://www.pivotaltracker.com/story/show/175806310)
    
    Co-authored-by: Renee Chu <rchu@vmware.com>

--- build/minio/build.sh
+++ build/minio/build.sh
@@ -5,6 +5,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 echo "Generating Minio resource definitions..."
 
-helm template cf-blobstore --namespace=cf-blobstore -f "${SCRIPT_DIR}/values.yml" "${SCRIPT_DIR}/_vendir/stable/minio/" |
+helm template cf-blobstore --namespace=cf-blobstore -f "${SCRIPT_DIR}/values.yml" "${SCRIPT_DIR}/_vendir/minio/" |
   ytt --ignore-unknown-comments -f - -f "${SCRIPT_DIR}/scrub_default_creds.yml" |
   kbld -f - > "${SCRIPT_DIR}/../../config/minio/_ytt_lib/minio/rendered.yml"

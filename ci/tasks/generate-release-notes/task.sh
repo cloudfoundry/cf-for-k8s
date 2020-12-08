01868dab5563e360038df31f805ca0d738290d7c
James Pollard <pollardja@vmware.com>
2020-12-07 18:36:35 -0800

    CI-ENH: auto generate release bump table for ship-it jobs
    [#175309371](https://www.pivotaltracker.com/story/show/175309371)
    
    Co-authored-by: Travis Patterson <travisp@vmware.com>

--- ci/tasks/generate-release-notes/task.sh
+++ ci/tasks/generate-release-notes/task.sh
@@ -15,6 +15,72 @@ function get_merged_prs() {
   done
 }
 
+function get_component_ref_from_vendir() {
+  local line_to_find="$1"
+  local vendir_path="$2"
+
+  grep -A 6 "${line_to_find}" ${vendir_path} | grep -E "ref|tag" | head -n1 | awk '{ print $2 }'
+}
+
+function append_component_line() {
+  local component_name="$1"
+  local line_to_find="$2"
+
+  from_sha=$(get_component_ref_from_vendir "${line_to_find}" cf-for-k8s-last-release/vendir.yml)
+  to_sha=$(get_component_ref_from_vendir "${line_to_find}" cf-for-k8s-last-rc/vendir.yml)
+  if [[ -z ${from_sha} ]] || [[ -z ${to_sha} ]]; then
+    echo "ERROR: Parsing of vendir version for component ${component_name} failed"
+    echo "exit 1"
+  elif [[ ${from_sha} == ${to_sha} ]]; then
+    echo "Before and after SHAs/refs match - no need to add to table"
+  else
+    if [[ "${component_name}" == "CF API" ]]; then
+      from_sha=$(echo "${from_sha}" | cut -c 1-7)
+      short_to_sha=$(echo "${to_sha}" | cut -c 1-7)
+      to_hyperlink="[${short_to_sha}](https://github.com/cloudfoundry/capi-k8s-release/commit/${to_sha})"
+    elif [[ "${component_name}" == "Networking" ]]; then
+      from_sha=$(echo "${from_sha}" | cut -c 1-7)
+      short_to_sha=$(echo "${to_sha}" | cut -c 1-7)
+      to_hyperlink="[${short_to_sha}](https://github.com/cloudfoundry/cf-k8s-networking/commit/${to_sha})"
+    elif [[ "${component_name}" == "Eirini" ]]; then
+      to_hyperlink="[${to_sha}](https://github.com/cloudfoundry-incubator/eirini-release/releases/tag/${to_sha})"
+    elif [[ "${component_name}" == "Kpack" ]]; then
+      to_hyperlink="[${to_sha}](https://github.com/pivotal/kpack/releases/tag/${to_sha})"
+    elif [[ "${component_name}" == "Logging" ]]; then
+      to_hyperlink="[${to_sha}](https://github.com/cloudfoundry/cf-k8s-logging/releases/tag/${to_sha})"
+    elif [[ "${component_name}" == "Metrics" ]]; then
+      to_hyperlink="[${to_sha}](https://github.com/cloudfoundry/metric-proxy/releases/tag/${to_sha})"
+    # elif [[ "${component_name}" == "QuarksSecret" ]]; then
+    #   to_hyperlink="[${to_sha}](https://github.com/cloudfoundry/uaa/releases/tag/${to_sha})"
+    elif [[ "${component_name}" == "UAA" ]]; then
+      to_hyperlink="[${to_sha}](https://github.com/cloudfoundry/uaa/releases/tag/${to_sha})"
+    else
+      echo "ERROR: Unrecognized component name: ${component_name}"
+      ex
+    fi
+    release_table_text+="\n| ${component_name} | ${from_sha} | ${to_hyperlink} |"
+  fi
+}
+
+function build_component_bump_table_content() {
+  release_table_text="
+| Release | Old Version | New Version |
+| ------- | ----------- | ----------- |"
+
+  append_component_line "CF API" "path: config/capi/_ytt_lib/capi-k8s-release"
+  append_component_line "Eirini" "path: build/eirini/_vendir"
+  append_component_line "Kpack" "path: config/kpack/_ytt_lib/kpack"
+  append_component_line "Logging" "path: config/logging/_ytt_lib/cf-k8s-logging"
+  append_component_line "Metrics" "path: config/metrics/_ytt_lib/metric-proxy"
+  append_component_line "Networking" "path: config/networking/_ytt_lib/cf-k8s-networking"
+  # append_component_line "QuarksSecret" "path: build/quarks-secret/_vendir"
+  # TODO make Quarks normal after the 1.1 release
+  release_table_text+="\n| QuarksSecret | n/a | [4171053](https://github.com/cloudfoundry-incubator/quarks-secret/commit/4171053d17e66a93044795924abdca66cac09785) |"
+  append_component_line "UAA" "path: config/uaa/_ytt_lib/uaa"
+  release_table_text+="\n"
+
+}
+
 function main() {
   local cwd="$1"
 
@@ -42,8 +108,7 @@ $(get_merged_prs "${GITHUB_API_USER}" "${GITHUB_API_TOKEN}" "${last_release_vers
 
 ## Release Updates
 
-| Release | Old Version | New Version |
-| ------- | ----------- | ----------- |
+$(printf "${release_table_text}")
 
 ## Contributors
 $(git log --format='%aN' "${last_release_version}...HEAD" | sort -u)
@@ -51,4 +116,4 @@ EOT
   popd > /dev/null
 }
 
-main "${PWD}"
+# main "${PWD}"

8f89325b2f25da67a59412918915e6815dd05d4c
James Pollard <pollardja@vmware.com>
2020-12-18 11:23:47 -0800

    ENH: use an annotated minio image
    - the image came from minio/minio:RELEASE.2020-06-14T18-32-17Z and has
    been annotated and moved into the not-rate-limited cloudfoundry
    dockerhub

--- build/minio/build.sh
+++ build/minio/build.sh
@@ -7,4 +7,5 @@ echo "Generating Minio resource definitions..."
 
 helm template cf-blobstore --namespace=cf-blobstore -f "${SCRIPT_DIR}/values.yml" "${SCRIPT_DIR}/_vendir/stable/minio/" |
   ytt --ignore-unknown-comments -f - -f "${SCRIPT_DIR}/scrub_default_creds.yml" |
-  kbld -f - > "${SCRIPT_DIR}/../../config/minio/_ytt_lib/minio/rendered.yml"
+  kbld -f "${SCRIPT_DIR}/osl-compliant-image-override.yml" -f - \
+  > "${SCRIPT_DIR}/../../config/minio/_ytt_lib/minio/rendered.yml"

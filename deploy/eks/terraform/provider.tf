14b8aa1fded11044ae7b7d438b6757f84b41daa7
Andrew Costa <ancosta@vmware.com>
2021-01-28 12:34:31 -0800

    Fix EKS terraform templates some more
    - Seems like this config is actually unused, so just removing it all together
    
    Co-authored-by: Andrew Costa <ancosta@vmware.com>
    Co-authored-by: Jaskanwal Pawar <jpawar@pivotal.io>

--- deploy/eks/terraform/provider.tf
+++ deploy/eks/terraform/provider.tf
@@ -6,7 +6,6 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  config_path            = "~/.kube/config"
   host                   = data.aws_eks_cluster.cluster.endpoint
   token                  = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)

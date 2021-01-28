ddb10c758b6751dc077fb20bcfac78f371191a9e
Andrew Costa <ancosta@vmware.com>
2021-01-28 12:01:27 -0800

    Fix EKS Terraform templates
    - Terraform Kubernetes provider upgrade introducing a breaking change in
    its config, so this addresses that
    
    Co-authored-by: Andrew Costa <ancosta@vmware.com>
    Co-authored-by: Jaskanwal Pawar <jpawar@pivotal.io>

--- deploy/eks/terraform/provider.tf
+++ deploy/eks/terraform/provider.tf
@@ -6,7 +6,7 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  load_config_file       = "false"
+  config_path            = "~/.kube/config"
   host                   = data.aws_eks_cluster.cluster.endpoint
   token                  = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)

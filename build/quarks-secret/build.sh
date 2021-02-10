2793e3ab5a49ea447328fa0484c5d4f0ec94b16b
James Pollard <pollardja@vmware.com>
2021-02-10 12:02:03 -0800

    MAINT: fix helm chart for QuarksSecret
    - helm is now more strict and doesn't allow "x.x.x" for the chart
    version
    - this fixes the error "Error: validation: chart.metadata.version
    "x.x.x" is invalid"
    - PR to quarks-secret: https://github.com/cloudfoundry-incubator/quarks-secret/pull/92
    
    Co-authored-by: Andrew Costa <ancosta@vmware.com>

--- build/quarks-secret/build.sh
+++ build/quarks-secret/build.sh
@@ -5,7 +5,20 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 echo "generating QuarksSecret resource definitions..."
 
+chart_yaml=${SCRIPT_DIR}/_vendir/deploy/helm/quarks-secret/Chart.yaml
+
+# won't be necessary after this PR is merged and we start using a QuarksSecret that includes the change:
+# https://github.com/cloudfoundry-incubator/quarks-secret/pull/92
+sed -i -r 's/x.x.x/0.0.0/g' ${chart_yaml}
+
+# some versions of sed create this strange file with a -r suffix
+if [[ -f ${SCRIPT_DIR}/_vendir/deploy/helm/quarks-secret/Chart.yaml-r ]]; then
+  rm ${SCRIPT_DIR}/_vendir/deploy/helm/quarks-secret/Chart.yaml-r
+fi
+
 helm template cf-quarks-secret --namespace=cf-system "${SCRIPT_DIR}/_vendir/deploy/helm/quarks-secret" \
   --values="${SCRIPT_DIR}/quarks-values.yaml" |
   ytt --ignore-unknown-comments -f - |
   kbld -f "${SCRIPT_DIR}/image-override.yml" -f - > "${SCRIPT_DIR}/../../config/quarks-secret/_ytt_lib/quarks-secret/rendered.yml"
+
+git checkout ${chart_yaml}

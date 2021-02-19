69ad4d25ab65b375987c76ff1d2fb038e33ade33
James Pollard <pollardja@vmware.com>
2021-02-18 17:39:52 -0800

    CI-FIX: only use cf-for-k8s-ci resource

--- ci/tasks/verify-every-task-is-used-in-a-pipeline/task.sh
+++ ci/tasks/verify-every-task-is-used-in-a-pipeline/task.sh
@@ -2,11 +2,11 @@
 
 set -u
 
-pushd cf-for-k8s-develop >> /dev/null
+pushd cf-for-k8s-ci >> /dev/null
   FAIL=false
 
   for task_file in ci/tasks/*/task.yml; do
-      ack "$task_file" ../$repo/ci >> /dev/null
+      ack "$task_file" ./ci >> /dev/null
       if [[ $? != 0 ]]; then
         FAIL=true
         echo "$task_file is not used in any pipeline"

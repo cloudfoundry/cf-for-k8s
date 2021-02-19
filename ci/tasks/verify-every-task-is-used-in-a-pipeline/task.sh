a23b2e589d4b5dfba84955232b06bc447304dea1
James Pollard <pollardja@vmware.com>
2021-02-18 17:27:16 -0800

    CI-ENH: add task to make sure all tasks are being used

---
+++ ci/tasks/verify-every-task-is-used-in-a-pipeline/task.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -u
+
+pushd cf-for-k8s-develop >> /dev/null
+  FAIL=false
+
+  for task_file in ci/tasks/*/task.yml; do
+      ack "$task_file" ../$repo/ci >> /dev/null
+      if [[ $? != 0 ]]; then
+        FAIL=true
+        echo "$task_file is not used in any pipeline"
+      fi
+
+  done
+
+  if $FAIL; then
+    exit 1
+  fi
+popd >> /dev/null

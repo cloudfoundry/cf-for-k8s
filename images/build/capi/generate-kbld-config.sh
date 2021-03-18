34e15a28c157bdb306138b4fe52c48bd622e762f
Andrew Costa <ancosta@vmware.com>
2021-03-17 17:13:36 -0700

    ENH: Config for building first two CAPI images
    - cloud-controller-ng and cf-api-controllers
    - remove accidentally committed kbld.lock.yml
    
    [#176852320](https://www.pivotaltracker.com/story/show/176852320)
    
    Co-authored-by: Dave Walter <walterda@vmware.com>

---
+++ images/build/capi/generate-kbld-config.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function generate_kbld_config() {
+  local kbld_config_path="${1}"
+
+  local source_path
+  source_path="${SCRIPT_DIR}/../../sources/cloud_controller_ng"
+
+  pushd "${source_path}" > /dev/null
+    local git_sha
+    git_sha=$(git rev-parse HEAD)
+  popd > /dev/null
+
+  echo "Creating CAPI K8s release kbld config with ytt..."
+  local kbld_config_values
+  kbld_config_values=$(cat <<EOF
+#@data/values
+---
+git_sha: ${git_sha}
+EOF
+)
+
+  local kbld_config
+  echo "${kbld_config_values}" | ytt -f "${SCRIPT_DIR}/kbld.yml" -f - > ${kbld_config_path}
+}
+
+function main() {
+  local kbld_config_path="${1}"
+
+  generate_kbld_config "${kbld_config_path}"
+}
+
+main "$@"

e680e70bb9087f1ab38f3eb25b9e73d4beafacd4
Andrew Costa <ancosta@vmware.com>
2021-03-23 14:44:14 -0600

    Migrate over networking images pipeline and tasks
    [#176354672](https://www.pivotaltracker.com/story/show/176354672)
    
    Signed-off-by: Caitlyn Yu <cyu@vmware.com>

---
+++ ci/tasks/cf-k8s-networking/update-image-digest-in-cf-for-k8s.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# ENV
+: "${COMPONENT_NAME:?}"
+: "${TARGET_FILE:?}"
+: "${BRANCH:?}"
+
+pushd image-resource > /dev/null
+  digest="$(cat digest)"
+popd
+
+pushd cf-for-k8s-develop
+  git checkout -b "${BRANCH}" develop
+
+  sed -r -i "s|(cf-k8s-networking/${COMPONENT_NAME})@sha256:[a-f0-9]+|\1@${digest}|" "${TARGET_FILE}"
+  ./build/istio/build.sh
+
+  git config user.name "${GIT_COMMIT_USERNAME}"
+  git config user.email "${GIT_COMMIT_EMAIL}"
+
+  if [[ -n $(git status --porcelain) ]]; then
+      echo "changes detected, will commit..."
+      git add "${TARGET_FILE}"
+      git add "config/istio/istio-generated"
+      git commit -m "Update ${COMPONENT_NAME} image digest to ${digest}"
+
+      git log -1 --color | cat
+  else
+      echo "no changes in repo, no commit necessary"
+  fi
+popd
+
+# include dot files in * globing
+shopt -s dotglob
+cp -r cf-for-k8s-develop/* cf-for-k8s-modified/

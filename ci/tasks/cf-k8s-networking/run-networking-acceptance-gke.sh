1a58e4a46c4ecb83c444f311e355f73c270e6551
Andrew Costa <ancosta@vmware.com>
2021-03-25 10:47:51 -0700

    CI-ENH: Use pools in cf-k8s-networking-istio-upgrade
    - update run-networking-acceptance-gke task accordingly
    
    [#176354672](https://www.pivotaltracker.com/story/show/176354672)
    
    Co-authored-by: Dave Walter <walterda@vmware.com>

--- ci/tasks/cf-k8s-networking/run-networking-acceptance-gke.sh
+++ ci/tasks/cf-k8s-networking/run-networking-acceptance-gke.sh
@@ -2,28 +2,22 @@
 
 set -euo pipefail
 
-source cf-for-k8s-ci/ci/tasks/cf-k8s-networking/helpers.sh
+source cf-for-k8s-ci/ci/helpers/gke.sh
 
 # ENV
-: "${INTEGRATION_CONFIG_FILE:?}"
-: "${CLOUDSDK_COMPUTE_REGION:?}"
-: "${CLOUDSDK_COMPUTE_ZONE:?}"
 : "${GCP_SERVICE_ACCOUNT_KEY:?}"
-: "${GCP_PROJECT:?}"
+: "${INTEGRATION_CONFIG_FILE:?}"
 
 function main() {
-    export KUBECONFIG=kubeconfig/config
+  local cluster_name
+  cluster_name="$(cat pool-lock/name)"
+  gcloud_auth "${cluster_name}"
 
-    gcloud auth activate-service-account --key-file=<(echo "${GCP_SERVICE_ACCOUNT_KEY}") --project="${GCP_PROJECT}" 1>/dev/null 2>&1
-    gcloud container clusters get-credentials ${CLUSTER_NAME} 1>/dev/null 2>&1
+  local config="${PWD}/integration-config/${INTEGRATION_CONFIG_FILE}"
 
-    local kubeconfig_path="${PWD}/${KUBECONFIG}"
-    local config="${PWD}/integration-config/${INTEGRATION_CONFIG_FILE}"
-
-    pushd cf-k8s-networking/test/acceptance > /dev/null
-        ./bin/test_local "${config}" "${kubeconfig_path}"
-    popd
+  pushd cf-k8s-networking/test/acceptance > /dev/null
+    ./bin/test_local "${config}" "${KUBECONFIG}"
+  popd
 }
 
-initialize_gke_env_vars
 main

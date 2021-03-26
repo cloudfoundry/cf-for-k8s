6ff991c4ea0cb4208b9679e01026557a903a4871
Andrew Wittrock <awittrock@vmware.com>
2021-03-26 14:05:50 -0700

    CI: Add values interface check
    - When previewing release notes

---
+++ ci/tasks/check-values-interface/task.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+function main() {
+    local cwd="$1"
+
+    local release_candidate_version
+    release_candidate_version="v$(cat cf-for-k8s-rc-version/version)"
+
+    local last_release_version
+    last_release_version=$(curl --silent "https://api.github.com/repos/cloudfoundry/cf-for-k8s/releases/latest" | jq -r .tag_name)
+
+    echo "Checking values interface for ${last_release_version} ---> ${release_candidate_version}"
+
+    pushd cf-for-k8s-last-release > /dev/null
+      ./hack/generate-values.sh -d wingdang-foobrizzle > ${cwd}/wingdang-foobrizzle-values.yml
+    popd > /dev/null
+
+    pushd cf-for-k8s-rc > /dev/null
+      ytt -f config/ -f ${cwd}/wingdang-foobrizzle-values.yml > /dev/null
+    popd > /dev/null
+}
+
+main "${PWD}"

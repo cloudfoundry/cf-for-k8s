2c74b89c1ee38cae0e9c337ac97c6acc6c4bbfe7
Andrew Wittrock <awittrock@vmware.com>
2021-03-29 17:05:07 -0700

    CI: Add sample values to check-values-interface
    Signed-off-by: Andrew Costa <ancosta@vmware.com>
    Co-authored-by: Andrew Costa <ancosta@vmware.com>

--- ci/tasks/check-values-interface/task.sh
+++ ci/tasks/check-values-interface/task.sh
@@ -14,11 +14,12 @@ function main() {
     echo "Checking values interface for ${last_release_version} ---> ${release_candidate_version}"
 
     pushd cf-for-k8s-last-release > /dev/null
+      cp ./sample-cf-install-values.yml ${cwd}/prev-release-sample-values.yml
       ./hack/generate-values.sh -d wingdang-foobrizzle > ${cwd}/wingdang-foobrizzle-values.yml
     popd > /dev/null
 
     pushd cf-for-k8s-rc > /dev/null
-      ytt -f config/ -f ${cwd}/wingdang-foobrizzle-values.yml > /dev/null
+      ytt -f config/ -f ${cwd}/wingdang-foobrizzle-values.yml -f ${cwd}/prev-release-sample-values.yml > /dev/null
     popd > /dev/null
 }
 

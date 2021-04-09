6f2122925b81c85277b3d78432b096176521dc46
Andrew Wittrock <awittrock@vmware.com>
2021-04-09 11:17:38 -0700

    Make quarks-secret image configurable with overlay
    - Remove kbld
    
    [#177585539](https://www.pivotaltracker.com/story/show/177585539)
    
    Co-authored-by: Andrew Costa <ancosta@vmware.com>

--- build/quarks-secret/build.sh
+++ build/quarks-secret/build.sh
@@ -18,7 +18,6 @@ fi
 
 helm template cf-quarks-secret --namespace=cf-system "${SCRIPT_DIR}/_vendir/deploy/helm/quarks-secret" \
   --values="${SCRIPT_DIR}/quarks-values.yaml" |
-  ytt --ignore-unknown-comments -f - |
-  kbld -f "${SCRIPT_DIR}/image-override.yml" -f - > "${SCRIPT_DIR}/../../config/quarks-secret/_ytt_lib/quarks-secret/rendered.yml"
+  ytt --ignore-unknown-comments -f - > "${SCRIPT_DIR}/../../config/quarks-secret/_ytt_lib/quarks-secret/rendered.yml"
 
 git checkout ${chart_yaml}

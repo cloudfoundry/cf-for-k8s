80dd3f2f6c2b9f679418c939a84c8925f15bd444
Dave Walter <walterda@vmware.com>
2021-04-29 17:08:27 -0700

    ENH: Add cf-k8s-logging kbld config
    - all images use the Go CNB, except cf-k8s-logging, which still uses the
    fluentd Dockerfile in the cf-k8s-logging repository
    
    [#177863522](https://www.pivotaltracker.com/story/show/177863522)

---
+++ images/build/logging/generate-kbld-config.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function generate_kbld_config() {
+  local kbld_config_path="${1}"
+
+  local source_path
+  source_path="${SCRIPT_DIR}/../../sources/logging"
+
+  pushd "${source_path}" > /dev/null
+    local git_ref
+    git_ref=$(git rev-parse HEAD)
+  popd > /dev/null
+
+  echo "Creating logging kbld config with ytt..."
+  local kbld_config_values
+  kbld_config_values=$(cat <<EOF
+#@data/values
+---
+git_ref: ${git_ref}
+git_url: https://github.com/cloudfoundry/cf-k8s-logging
+EOF
+)
+
+  echo "${kbld_config_values}" | ytt -f "${SCRIPT_DIR}/kbld.yml" -f - > "${kbld_config_path}"
+}
+
+function main() {
+  local kbld_config_path="${1}"
+
+  generate_kbld_config "${kbld_config_path}"
+}
+
+main "$@"

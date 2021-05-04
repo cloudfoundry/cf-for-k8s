cad28019941330691725901fba2989d6a1fb4a8f
Andrew Wittrock <awittrock@vmware.com>
2021-05-04 16:18:10 -0700

    ENH: Add an Istio version label to all system components
    - this will cause kapp to roll the compoenents whenever the Istio
    version changes, which will result in the new version of the proxy
    sidecar being injected into the pods
    
    [#177914742](https://www.pivotaltracker.com/story/show/177914742)
    
    Co-authored-by: Dave Walter <walterda@vmware.com>

--- build/istio/build.sh
+++ build/istio/build.sh
@@ -5,7 +5,11 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 ${SCRIPT_DIR}/generate.sh "$@" | kbld -f - > "${SCRIPT_DIR}/../../config/istio/istio-generated/xxx-generated-istio.yaml"
 
-# save the current Istio version in the networking configs
+# Save the current Istio version in the networking configs
 # NOTE: this project uses python yq module (https://kislyuk.github.io/yq/)
 ISTIO_VERSION="$(< "${SCRIPT_DIR}/values.yaml" yq -r .istio_version)"
-sed -r -i'' 's/(return)(.*\..*\..*)$/\1 "'${ISTIO_VERSION}'"/' "${SCRIPT_DIR}/../../config/istio/upgrade-istio-sidecars-job.yml"
+cat <<EOF > "${SCRIPT_DIR}/../../config/istio/istio-version.star"
+def istio_version():
+  return "${ISTIO_VERSION}"
+end
+EOF
---
+++ config/istio/istio-version.star
@@ -0,0 +1,3 @@
+def istio_version():
+  return "1.9.4"
+end

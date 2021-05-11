56c4f6f1232728c643404b206871c57c6f560975
Dave Walter <walterda@vmware.com>
2021-05-11 14:33:01 -0700

    CI-FIX: Use `git rev-parse` to retrieve git sha
    ... in generate-oci-image-labels task
    
    [#177974517](https://www.pivotaltracker.com/story/show/177974517)

--- ci/tasks/generate-oci-image-labels/task.sh
+++ ci/tasks/generate-oci-image-labels/task.sh
@@ -2,14 +2,19 @@
 
 set -euo pipefail
 
+function parse_git_ref() {
+  pushd source-repo >/dev/null
+    git rev-parse HEAD
+  popd >/dev/null
+}
+
 function parse_git_source() {
   pushd source-repo >/dev/null
     git remote get-url origin
   popd >/dev/null
 }
 
-# Manually parse revision info from input git resource
-git_ref=$(cat source-repo/.git/ref)
+git_ref=$(parse_git_ref)
 git_source=$(parse_git_source)
 
 cat <<EOF > labels/oci-image-labels

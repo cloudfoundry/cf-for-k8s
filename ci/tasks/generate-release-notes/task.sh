1d3c2abfe0f80d5e0366788ed69490d2da9e42fb
Andrew Wittrock <awittrock@vmware.com>
2021-05-12 16:51:17 -0700

    Update release notes generator for Istio 1.9.5

--- ci/tasks/generate-release-notes/task.sh
+++ ci/tasks/generate-release-notes/task.sh
@@ -91,7 +91,7 @@ function build_component_bump_table_content() {
   append_component_line "CF API" "path: config/capi/_ytt_lib/capi-k8s-release"
   append_component_line "Eirini" "path: build/eirini/_vendir"
   # TODO: Make versions dynamic by fetching from source of truth.
-  release_table_text+="\n| Istio | 1.8.4 | [1.9.4](https://github.com/istio/istio/releases/tag/1.9.4) |"
+  release_table_text+="\n| Istio | 1.9.4 | [1.9.5](https://github.com/istio/istio/releases/tag/1.9.5) |"
   append_component_line "Kpack" "path: config/kpack/_ytt_lib/kpack"
   append_component_line "Logging" "path: config/logging/_ytt_lib/cf-k8s-logging"
   append_component_line "Metrics" "path: config/metrics/_ytt_lib/metric-proxy"

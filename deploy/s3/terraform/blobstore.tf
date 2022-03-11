c0b2c8f5eb849129b54a1d3098264b36e82ef222
Dave Walter <walterda@vmware.com>
2022-03-11 15:09:06 -0800

    CI-FIX: Update S3 terraform templates
    - the ACL is now defined via a separate "aws_s3_bucket_acl" resource

--- deploy/s3/terraform/blobstore.tf
+++ deploy/s3/terraform/blobstore.tf
@@ -1,23 +1,39 @@
 resource "aws_s3_bucket" "packages" {
   bucket        = "cc-packages-${var.bucket_suffix}"
-  acl           = "private"
   force_destroy = true
+}
+
+resource "aws_s3_bucket_acl" "packages" {
+  bucket = aws_s3_bucket.packages.id
+  acl    = "private"
 }
 
 resource "aws_s3_bucket" "droplets" {
   bucket        = "cc-droplets-${var.bucket_suffix}"
-  acl           = "private"
   force_destroy = true
+}
+
+resource "aws_s3_bucket_acl" "droplets" {
+  bucket = aws_s3_bucket.droplets.id
+  acl    = "private"
 }
 
 resource "aws_s3_bucket" "resources" {
   bucket        = "cc-resources-${var.bucket_suffix}"
-  acl           = "private"
   force_destroy = true
+}
+
+resource "aws_s3_bucket_acl" "resources" {
+  bucket = aws_s3_bucket.resources.id
+  acl    = "private"
 }
 
 resource "aws_s3_bucket" "buildpacks" {
   bucket        = "cc-buildpacks-${var.bucket_suffix}"
-  acl           = "private"
   force_destroy = true
+}
+
+resource "aws_s3_bucket_acl" "buildpacks" {
+  bucket = aws_s3_bucket.buildpacks.id
+  acl    = "private"
 }

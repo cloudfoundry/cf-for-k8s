d920c87f023c6194c8c23bc0359959147e95a825
Dave Walter <walterda@vmware.com>
2022-03-14 17:23:46 -0700

    Switch to a managed EKS node group

--- deploy/eks/terraform/eks.tf
+++ deploy/eks/terraform/eks.tf
@@ -7,13 +7,49 @@ module "eks" {
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
 
-  self_managed_node_groups = {
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  cluster_addons = {
+    coredns = {
+      resolve_conflicts = "OVERWRITE"
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      resolve_conflicts = "OVERWRITE"
+    }
+  }
+
+  eks_managed_node_group_defaults = {
+    instance_type = var.node_machine_type
+  }
+
+  eks_managed_node_groups = {
     worker_group = {
-      name          = "worker-group"
-      instance_type = var.node_machine_type
-      desired_size  = var.node_count
-      max_size      = var.node_count
-      min_size      = var.node_count
+      desired_size   = var.node_count
+      max_size       = var.node_count
+      min_size       = var.node_count
+    }
+  }
+
+  node_security_group_additional_rules = {
+    ingress_self_all = {
+      description = "Node to node all ports/protocols"
+      protocol    = "-1"
+      from_port   = 0
+      to_port     = 0
+      type        = "ingress"
+      self        = true
+    }
+
+    egress_all = {
+      description      = "Node all egress"
+      protocol         = "-1"
+      from_port        = 0
+      to_port          = 0
+      type             = "egress"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
     }
   }
 
--- deploy/eks/terraform/vpc.tf
+++ deploy/eks/terraform/vpc.tf
@@ -8,6 +8,7 @@ module "vpc" {
   public_subnets       = var.public_subnets
   enable_nat_gateway   = true
   single_nat_gateway   = true
+  enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
